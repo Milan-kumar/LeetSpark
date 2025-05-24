5c9eef608a3d471a97c561e781d858a32453d9ae
Milan Kumar <milanKryadav@gmail.com>
2025-05-24 14:03:04 +0100

    added update problem and frontend signup and Login

--- backend/src/routes/problem.route.js
+++ backend/src/routes/problem.route.js
@@ -1,17 +1,22 @@
 import express from "express";
 import { authMiddleware, checkAdmin } from "../middleware/auth.middleware.js";
-import { createProblem, deleteProblem, getAllProblems, getAllProblemsSolvedByUser, getProblem } from "../controllers/problem.controller.js";
+import { createProblem, deleteProblem, getAllProblems, getAllProblemsSolvedByUser, getProblem, updateProblem } from "../controllers/problem.controller.js";
 
 const problemRoutes = express.Router();
 
+// Create a new problem (Admin only)
 problemRoutes.post("/create-problem", authMiddleware, checkAdmin, createProblem);
 
+// Get all problems (Public)
 problemRoutes.get("/get-all-problems", authMiddleware, getAllProblems);
 
+// Get a problem by ID (Public)
 problemRoutes.get("/get-problem/:id", authMiddleware, getProblem)
 
-// problemRoutes.post("/update-problem/:id", authMiddleware, checkAdmin, updateProblem);
+// Update a problem by ID (Admin only) put is used TO UPDATE ALL FIELDS AND PATCH IS USED TO UPDATE SPECIFIC FIELDS ONLY
+problemRoutes.post("/update-problem/:id", authMiddleware, checkAdmin, updateProblem);
 
+// Delete a problem by ID (Admin only)
 problemRoutes.delete("/delete-problem/:id", authMiddleware, checkAdmin, deleteProblem);
 
 problemRoutes.get("/get-solved-problems", authMiddleware, getAllProblemsSolvedByUser);
