8f46800d26c133948172707ef6662ab37fce26da
Milan Kumar <milanKryadav@gmail.com>
2025-07-10 00:51:44 +0100

    fixed server url

--- frontend/src/libs/axios.js
+++ frontend/src/libs/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: import.meta.env.MODE === "development" ? `http://localhost:8081/api/v1` : `${VITE_SERVER_URL}/api/v1`,
+    baseURL: import.meta.env.MODE === "development" ? `http://localhost:8081/api/v1` : `${import.meta.env.VITE_SERVER_URL}/api/v1`,
     withCredentials: true,
 });
