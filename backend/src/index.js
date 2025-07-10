4facb2b503fec2278a2bfddf6ed309c841cc79e1
Milan Kumar <milankryadav@gmail.com>
2025-07-10 13:10:00 +0100

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -17,7 +17,7 @@ const app = express();
 //middleware
 // origin: "http://localhost:5173",
 app.use(cors({
-    origin: process.env.APP_URL,
+    origin: "https://www.leetspark.xyz/",
     credentials: true
 }))
 
